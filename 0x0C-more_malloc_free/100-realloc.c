bf72f4388a9164fbb43124c2756293e1c92be896
Natacha-07 <akinloyeoluwakemi07@gmail.com>
2022-04-07 11:15:08 -0700

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,54 +2,48 @@
 #include <stdlib.h>
 
 /**
- * _realloc - Reallocates a memory block using malloc and free.
- * @ptr: A pointer to the memory previously allocated.
- * @old_size: The size in bytes of the allocated space for ptr.
- * @new_size: The size in bytes for the new memory block.
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
  *
- * Return: If new_size == old_size - ptr.
- *         If new_size == 0 and ptr is not NULL - NULL.
- *         Otherwise - a pointer to the reallocated memory block.
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	void *mem;
-	char *ptr_copy, *filler;
-	unsigned int index;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
-	if (new_size == old_size)
-		return (ptr);
+	if (s1 == NULL)
+		s1 = "";
 
-	if (ptr == NULL)
-	{
-		mem = malloc(new_size);
+	if (s2 == NULL)
+		s2 = "";
 
-		if (mem == NULL)
-			return (NULL);
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
 
-		return (mem);
-	}
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
 
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
+	if (n > ls2)
+		n = ls2;
+
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
 		return (NULL);
-	}
 
-	ptr_copy = ptr;
-	mem = malloc(sizeof(*ptr_copy) * new_size);
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
 
-	if (mem == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
+	sout[i] = '\0';
 
-	filler = mem;
-
-	for (index = 0; index < old_size && index < new_size; index++)
-		filler[index] = *ptr_copy++;
-
-	free(ptr);
-	return (mem);
+	return (sout);
 }
